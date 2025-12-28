d696bbfc0a2e8307dfe29a6a3f99015bb8525762
Joseph Rey R. Panti <josephreyramirezpanti+github@gmail.com>
2025-12-28 16:16:41 +0800

    Added a method to extract and write data to a text file (as a form of database). For users.

--- joseph.org.rsm/src/main/java/org/joseph/DAO/UserDAO.java
+++ joseph.org.rsm/src/main/java/org/joseph/DAO/UserDAO.java
@@ -1,4 +1,95 @@
 package org.joseph.DAO;
 
-public class UserDAO {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.joseph.Model.Types.UserType;
+import org.joseph.Model.User;
+import org.joseph.Store.UserStore;
+
+public class UserDAO extends AbstractDAO {
+    private final String[] columns = new String[] {
+            "userId",
+            "username",
+            "password",
+            "first_name",
+            "last_name",
+            "type"
+    };
+
+    public UserDAO(String fileName) {
+        super(fileName);
+
+    }
+
+    @Override
+    protected void readData() {
+        List<String[]> data = new ArrayList<>();
+        List<User> users = new ArrayList<>();
+
+        try (Scanner reader = new Scanner(database)) {
+            reader.nextLine();
+            while(reader.hasNext()) {
+                data.add(reader.nextLine().split(","));
+            }
+        }
+        catch (IOException ex) {
+            System.out.println("Error: Cannot read file.");
+        }
+
+        if(data.isEmpty()) return;
+
+        // Create the User data object;
+        for(String[] datum : data) {
+            int userId = Integer.parseInt(datum[0]);
+            String username = datum[1];
+            String password = datum[2];
+            String fName = datum[3];
+            String lName = datum[4];
+            UserType type = UserType.fromString(datum[5]);
+
+            User user = new User(userId);
+            user.setUsername(username);
+            user.setPassword(password);
+            user.setFName(fName);
+            user.setLName(lName);
+            user.setType(type);
+
+            users.add(user);
+        }
+
+        UserStore.setUser(users);
+    }
+
+    @Override
+    protected void writeData() {
+        List<User> users = UserStore.getUser();
+        List<String[]> data = new ArrayList<>();
+
+        for(User user: users) {
+            String[] datum = new String[columns.length];
+            datum[0] = String.valueOf(user.getID());
+            datum[1] = user.getUsername();
+            datum[2] = user.getPassword();
+            datum[3] = user.getFName();
+            datum[4] = user.getLName();
+            datum[5] = user.getType().toString();
+            data.add(datum);
+        }
+
+        try(FileWriter writer = new FileWriter(database)) {
+            writer.append(String.join(",", columns)).append("\n");
+            for(String[] datum : data) {
+                writer.append(String.join(",", datum)).append("\n");
+            }
+        }
+        catch (IOException ex) {
+            System.out.println("Error cannot write data to " + database.getPath());
+        }
+    }
+
+
 }
