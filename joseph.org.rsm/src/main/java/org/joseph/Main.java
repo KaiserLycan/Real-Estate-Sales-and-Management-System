59c054c4142a50d5406b457bcaa8114b85250f6a
Joseph Rey R. Panti <josephreyramirezpanti+github@gmail.com>
2025-12-29 05:38:57 +0800

    Made Main.main Invoke AuthWindow.java

--- joseph.org.rsm/src/main/java/org/joseph/Main.java
+++ joseph.org.rsm/src/main/java/org/joseph/Main.java
@@ -1,7 +1,17 @@
 package org.joseph;
 
+import org.joseph.View.AuthWindow;
+
+import java.awt.EventQueue;
+
+
 public class Main {
     public static void main(String[] args) {
-
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new AuthWindow().setVisible(true);
+            }
+        });
     }
 }
