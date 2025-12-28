12dc84c26d22f891a6f56087aa1479d088a7934b
Joseph Rey R. Panti <josephreyramirezpanti+github@gmail.com>
2025-12-29 05:30:39 +0800

    Revised Invoice.java

--- joseph.org.rsm/src/main/java/org/joseph/Model/Invoice.java
+++ joseph.org.rsm/src/main/java/org/joseph/Model/Invoice.java
@@ -1,14 +1,26 @@
 package org.joseph.Model;
 
+import org.joseph.Model.Types.PurchaseStatus;
+import org.joseph.Model.Types.UserType;
+
+import java.time.LocalDate;
+
 public class Invoice {
     User agent;
+    User client;
     Lot lot;
+    LocalDate date_issued;
     float total_price;
 
-    public Invoice(User agent, User client, Lot lot) {
+    public Invoice(User agent, User client, Lot lot, LocalDate date_issued) throws Exception {
+        if(agent.getType() != UserType.ADMIN || client.getType() != UserType.CLIENT) {
+            throw new Exception("Invalid user type");
+        }
+
         this.agent = agent;
+        this.client = client;
+        this.date_issued = date_issued;
         this.lot = lot;
         total_price = lot.getPrice() + (lot.getHouse().getFixedPrice() > 0 ? lot.getPrice() + lot.getHouse().getFixedPrice() : 0);
     }
-
 }
