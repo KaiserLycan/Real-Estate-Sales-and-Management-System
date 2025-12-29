732e4ccb1a9185c2e8e6ec79748e37eedc74e99d
Joseph Rey R. Panti <josephreyramirezpanti+github@gmail.com>
2025-12-30 05:58:01 +0800

    Displayed user data in UsersView.java

--- joseph.org.rsm/src/main/java/org/joseph/View/UsersView.java
+++ joseph.org.rsm/src/main/java/org/joseph/View/UsersView.java
@@ -1,11 +1,44 @@
 package org.joseph.View;
 
+import org.joseph.Store.UserStore;
+
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.table.DefaultTableModel;
 
 public class UsersView extends JPanel {
 
+
+    private final JTable table;
+    private final DefaultTableModel model;
+
     public UsersView() {
-        setBackground(Color.BLUE);
+        String[] headers = new String[] {
+                "ID",
+                "Username",
+                "Full Name",
+                "Password",
+                "Permissions"
+        };
+
+        model = new DefaultTableModel(headers, 0);
+
+        UserStore.getInstance().getUser().forEach(user -> {
+            model.addRow(new Object[] {
+                    user.getID(),
+                    user.getUsername(),
+                    user.getFName() + " " + user.getLName(),
+                    user.getPassword(),
+                    user.getType()
+            });
+        });
+
+        table = new JTable(model);
+        JScrollPane scrollPane  = new JScrollPane(table);
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        add(scrollPane);
     }
+
+    public JTable getTable() { return table; }
+    public DefaultTableModel getModel() { return model; }
 }
