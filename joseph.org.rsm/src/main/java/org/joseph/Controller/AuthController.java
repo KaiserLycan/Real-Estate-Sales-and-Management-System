1665fb8b73e1429367719f63f5f80978d15e49ac
Joseph Rey R. Panti <josephreyramirezpanti+github@gmail.com>
2026-01-04 07:19:02 +0800

    update (authentication): Streamlined the authentication process to allow login functionality when pressing enter.

--- joseph.org.rsm/src/main/java/org/joseph/Controller/AuthController.java
+++ joseph.org.rsm/src/main/java/org/joseph/Controller/AuthController.java
@@ -10,39 +10,50 @@ import org.joseph.View.ClientFrame;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
-public class AuthController {
+public class AuthController{
+
+    AuthFrame frame;
 
     public AuthController(AuthFrame frame) {
+        this.frame = frame;
+
         frame.getLoginButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                User user = new User(0, false);
-                JFrame toOpen;
-
-                user.setPassword(((JTextField) frame.getPasswordField()).getText());
-                user.setUsername(frame.getUsernameField().getText());
-
-                User authenticatedUser = AuthenticateUser.authenticateUser(user);
-                if(authenticatedUser == null) {
-                    frame.popMessage("Invalid Username and Password", JOptionPane.ERROR_MESSAGE);
-                }
-                else {
-                    frame.popMessage("User login successfully.", JOptionPane.PLAIN_MESSAGE);
-                    if(authenticatedUser.getType() == UserType.ADMIN) {
-                        toOpen = new AdminFrame();
-                        toOpen.setVisible(true);
-                        toOpen.setExtendedState(JFrame.MAXIMIZED_BOTH);
-                    }
-                    else {
-                        toOpen = new ClientFrame();
-                        toOpen.setVisible(true);
-                        toOpen.setExtendedState(JFrame.MAXIMIZED_BOTH);
-                    }
-
-                    frame.dispose();
-                }
+                performAuthentication();
             }
         });
+
+        frame.getRootPane().setDefaultButton(frame.getLoginButton());
+    }
+
+    private void performAuthentication() {
+        User user = new User(0, false);
+        JFrame toOpen;
+
+        user.setPassword(((JTextField) frame.getPasswordField()).getText());
+        user.setUsername(frame.getUsernameField().getText());
+
+        User authenticatedUser = AuthenticateUser.authenticateUser(user);
+        if(authenticatedUser == null) {
+            frame.popMessage("Invalid Username and Password", JOptionPane.ERROR_MESSAGE);
+        }
+        else {
+            frame.popMessage("User login successfully.", JOptionPane.PLAIN_MESSAGE);
+            if(authenticatedUser.getType() == UserType.ADMIN) {
+                toOpen = new AdminFrame();
+                toOpen.setVisible(true);
+                toOpen.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            }
+            else {
+                toOpen = new ClientFrame();
+                toOpen.setVisible(true);
+                toOpen.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            }
+
+            frame.dispose();
+        }
     }
 }
